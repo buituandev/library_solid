44ac4c3fe6592de4a730ddabf8fe1377ab1f7251
TUAN <buituan0303@tuan03.onmicrosoft.com>
2025-02-16 16:24:11 +0700

    Refactor BookRepository: replace direct field access with method parameter for better encapsulation

--- LibraryManagementSystem_BuiAnhTuan_CSE422/Repositories/BookRepository.cs
+++ LibraryManagementSystem_BuiAnhTuan_CSE422/Repositories/BookRepository.cs
@@ -4,41 +4,39 @@ namespace LibraryManagementSystem_BuiAnhTuan_CSE422.Repositories
 {
     public class BookRepository(List<IBook> books) : IBookRepository
     {
-        public List<IBook> Books = books;
-
         public void AddBook(IBook book)
         {
-            Books.Add(book);
+            books.Add(book);
         }
 
         public IBook? GetBook(string bookId)
         {
-            return (from book in Books where book.Id == bookId select book).FirstOrDefault();
+            return (from book in books where book.Id == bookId select book).FirstOrDefault();
         }
 
         public List<IBook> GetBookByAuthor(string author)
         {
-            return Books.Where(book => book.Author == author).ToList();
+            return books.Where(book => book.Author == author).ToList();
         }
 
         public List<IBook> GetBookByTitle(string title)
         {
-            return Books.Where(book => book.Title == title).ToList();
+            return books.Where(book => book.Title == title).ToList();
         }
 
         public void RemoveBook(IBook book)
         {
-            Books.Remove(book);
+            books.Remove(book);
         }
 
         public void UpdateBookQuantity(string bookId, int quantity)
         {
-            Books.FindAll(book => book.Id == bookId).ForEach(book => book.Quantity = quantity);
+            books.FindAll(book => book.Id == bookId).ForEach(book => book.Quantity = quantity);
         }
 
         public List<IBook> GetAllBooks()
         {
-            return Books;
+            return books;
         }
     }
 }
